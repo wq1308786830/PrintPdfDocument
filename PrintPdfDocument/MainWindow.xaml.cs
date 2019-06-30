cc07341067fec90c77bfc132eba1ffa656ec840a
Abraham Russell <flyingcycling@outlook.com>
2019-06-30 21:51:58 +0800

    page navigation ability

--- PrintPdfDocument/MainWindow.xaml.cs
+++ PrintPdfDocument/MainWindow.xaml.cs
@@ -1,115 +1,15 @@
-﻿using System.IO;
-using System.Windows;
-using System;
-using System.Drawing;
-using System.Drawing.Printing;
-using MessageBox = System.Windows.MessageBox;
-using PdfiumViewer;
+﻿using System.Windows.Navigation;
 
 namespace PrintPdfDocument
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
-        private Font printFont;
-        private StreamReader streamToPrint;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                streamToPrint = new StreamReader
-                 ("C:\\Users\\flyin\\Desktop\\testwifi.txt");
-                try
-                {
-                    printFont = new Font("宋体", 10);
-                    PrintDocument pd = new PrintDocument();
-                    pd.PrintPage += new PrintPageEventHandler
-                       (this.pd_PrintPage);
-                    pd.Print();
-                }
-                finally
-                {
-                    streamToPrint.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-        // The PrintPage event is raised for each page to be printed.
-        private void pd_PrintPage(object sender, PrintPageEventArgs ev)
-        {
-            float linesPerPage = 0;
-            float yPos = 0;
-            int count = 0;
-            float leftMargin = ev.MarginBounds.Left;
-            float topMargin = ev.MarginBounds.Top;
-            string line = null;
-
-            // Calculate the number of lines per page.
-            linesPerPage = ev.MarginBounds.Height /
-               printFont.GetHeight(ev.Graphics);
-
-            // Print each line of the file.
-            while (count < linesPerPage &&
-               ((line = streamToPrint.ReadLine()) != null))
-            {
-                yPos = topMargin + (count *
-                   printFont.GetHeight(ev.Graphics));
-                ev.Graphics.DrawString(line, printFont, Brushes.Black,
-                   leftMargin, yPos, new StringFormat());
-                count++;
-            }
-
-            // If more lines exist, print another page.
-            if (line != null)
-                ev.HasMorePages = true;
-            else
-                ev.HasMorePages = false;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                PrintDocument pd = new PrintDocument();
-                pd.PrintPage += (s, args) =>
-                {
-                    System.Drawing.Image i = System.Drawing.Image.FromFile("C:\\Users\\flyin\\Desktop\\雯雯小可爱\\2a4e5444eb1c8ca3.jpg");
-                    System.Drawing.Rectangle m = args.PageBounds;
-                    args.Graphics.DrawImage(i, m);
-                };
-                pd.Print();
-            } catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            string path = @"C:\\Users\\flyin\\Desktop\\ReferenceCard.pdf";
-            using (var document = PdfDocument.Load(path))
-            {
-                using (var printDocument = document.CreatePrintDocument())
-                {
-                    printDocument.PrinterSettings.PrintFileName = "Letter_SkidTags_Report_9ae93aa7-4359-444e-a033-eb5bf17f5ce6.pdf";
-                    printDocument.PrinterSettings.PrinterName = @"OneNote";
-                    printDocument.DocumentName = "file.pdf";
-                    printDocument.PrinterSettings.PrintFileName = "file.pdf";
-                    printDocument.PrintController = new StandardPrintController();
-                    printDocument.Print();
-                }
-            }
         }
     }
 }
